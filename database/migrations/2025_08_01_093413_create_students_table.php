acfc39cb8ba5fac44c99bbbd16520c473f0f4f12
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-08-01 11:06:22 +0000

    Aug 1, 2025, 12:06 PM

--- database/migrations/2025_08_01_093413_create_students_table.php
+++ database/migrations/2025_08_01_093413_create_students_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('people', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
