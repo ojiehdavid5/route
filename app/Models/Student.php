4378e5e4a2e37e9f62c5faa6fa84fd2f26ef3987
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-08-01 09:46:14 +0000

    Aug 1, 2025, 10:46 AM

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,11 +1,14 @@
 <?php
 
+// app/Models/Student.php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
-    
+    // ✅ Add this to allow mass assignment
+    protected $fillable = ['name', 'email', 'age'];
 }
+
